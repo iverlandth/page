d914cf54031b4ae1c7bb33ff53884cecd359b54b
iverlandth <iverherlandth@gmail.com>
2012-12-28 13:51:37 -0400

    blogkyoir

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogkyoir::Application.config.secret_token = 'f1f096fe5c9e0e77ffb22edbfa60f6aadb745606b9ce2fdc799a1b573f0f9cea206071005b2c198660e72fdb483aa7eefea681ccdb40459ef52e3c46722af5d2'
