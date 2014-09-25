668676e0753ccea5daca22c425597bea2abfe889
iverlandth <iverherlandth@gmail.com>
2014-09-25 18:46:35 -0300

    update page 25 de sept 2

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'sass-rails', '~> 4.0.2'
 
 gem 'bootstrap-sass'
 
+gem 'font-awesome-sass'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140724194232) do
+ActiveRecord::Schema.define(version: 20140925175638) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "contacts", force: true do |t|
-    t.string   "title"
     t.string   "name"
-    t.text     "content"
+    t.string   "email"
+    t.text     "message"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
